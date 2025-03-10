7c039c44ce13ca7fc8aa7bf3c083841263666697
Piyush Agrawal <piyushagrawal8726@gmail.com>
2025-03-10 09:57:32 +0530

    LOGIN and signup

--- app.py
+++ app.py
@@ -2,13 +2,40 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-
-
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 app = Flask(__name__)
 app.secret_key = 'supersecretmre'
 
+# Database Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
+# User Model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    user_type = db.Column(db.String(20), nullable=False)
+    
+    def __init__(self, username, email, password, user_type):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.user_type = user_type
+    
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Home Route
 @app.route('/')
 def home():
     flash('Welcome to the Flask App', 'info')
