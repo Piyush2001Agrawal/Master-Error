513ba5a8a740a4ae9f33e67c5e6af40cc5821f5f
Harsh <rajkumarkoylamandi@gmail.com>
2025-04-11 11:41:39 +0530

    #added model

--- geminiapi.py
+++ geminiapi.py
@@ -10,3 +10,42 @@ generation_config =  dict(
     
 )
 
+
+def executor():
+    model = genai.GenerativeModel(
+        model_name = "gemini-2.0-flash-exp",
+        generation_config=generation_config,
+        system_instruction=(
+            """
+Assume you are a universal code executor:
+- Detect the code language and display it.
+- Execute the code and provide the output.
+- If error occurs, then give output of the error.
+- If error comes include potential errors and improvements.
+"""
+        )
+    )
+
+    return model
+
+
+def analyzer():
+    model = genai.GenerativeModel(
+        model_name = "gemini-2.0-flash-exp",
+        generation_config=generation_config,
+        system_instruction=(
+            """
+Assume you are a code generator:
+- keep output simple and organized.
+- if user inputs any code, detect the code language and give suggestions for possible errors.
+- Provide correction code using optimized functions or classes.
+- Always include potential errors and improvements.
+- Summarize the output and incluide examples with both functions and classes and some.
+- if user input any code \ndetect the code language and give suggestions of possible errors and give correction code using optimized functions or classes\n- give us possible errors\n- and give the improvemnet in functions OR classses always for every code \n- organized and summarized the output in simple way and also include one example with function and one with class\n- more simple it and short it\n- need not to print Next Steps:\n\nI am ready to provide more details on more complex code.\n If programming language doesnot support function or class then show the best way to implement it with other languages. and give a simplest summary why use this language.\nI am ready to give potential error and improvements for complex code.\n
+"""
+        )
+
+    )
+
+    return model
+
